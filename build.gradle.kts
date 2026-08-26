d159b73c5e9b62c907dcba0baf9c5d4493a8e67b
lighttigerxiv <lighttigerxiv@gmail.com>
2026-08-26 17:53:53 +0100

    fix(*): made things public i hope

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "org.whiskersapps"
-version = "0.1.0"
+version = "0.1.1"
 
 repositories {
     mavenCentral()
@@ -18,7 +18,7 @@ dependencies {
 }
 
 kotlin {
-    jvmToolchain(25)
+    jvmToolchain(21)
 }
 
 publishing{
